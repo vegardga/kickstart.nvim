99cf6b4d7b4d006a038244b770ed4af29efd3c7c
Vegard Gamnes <vegard@gamnes.no>
2025-01-27 10:37:06 +0100

    Adjust colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -3,7 +3,14 @@ return {
   'folke/tokyonight.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    vim.cmd.colorscheme 'tokyonight-night'
+    vim.cmd.colorscheme 'tokyonight-moon'
     vim.cmd.hi 'Comment gui=none'
   end,
 }
+-- return {
+--   'rebelot/kanagawa.nvim',
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme 'kanagawa-dragon'
+--   end,
+-- }
