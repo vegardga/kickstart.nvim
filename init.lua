90fc1094c5f2e3754c7604495300e1b8f527147d
Vegard Gamnes <vegard@gamnes.no>
2025-01-29 13:36:43 +0100

    Center screen after up/down movement

--- init.lua
+++ init.lua
@@ -55,6 +55,8 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 vim.keymap.set('n', '<leader>W', '<cmd>w<CR>', { desc = 'Save file' })
 vim.keymap.set('n', '<leader>Q', '<cmd>qa<CR>', { desc = '[Q]uit all' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
 
 -- [[ Basic Autocommands ]]
 vim.api.nvim_create_autocmd('TextYankPost', {
