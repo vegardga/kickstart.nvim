fb1c994d51e94eea1c6b85cf66b3a54b774a0659
Vegard Gamnes <vegard@gamnes.no>
2025-01-29 13:36:29 +0100

    Add colorshceme rose-pine

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
-return {
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'folke/tokyonight.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    vim.cmd.colorscheme 'tokyonight-moon'
-    vim.cmd.hi 'Comment gui=none'
-  end,
-}
+-- return {
+--   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+--   'folke/tokyonight.nvim',
+--   priority = 1000, -- Make sure to load this before all the other start plugins.
+--   init = function()
+--     vim.cmd.colorscheme 'tokyonight-moon'
+--     vim.cmd.hi 'Comment gui=none'
+--   end,
+-- }
 -- return {
 --   'rebelot/kanagawa.nvim',
 --   priority = 1000,
@@ -14,3 +14,12 @@ return {
 --     vim.cmd.colorscheme 'kanagawa-dragon'
 --   end,
 -- }
+return {
+  'rose-pine/neovim',
+  name = 'rose-pine',
+  priority = 1000,
+  config = function()
+    vim.cmd 'colorscheme rose-pine-moon'
+    -- vim.cmd 'colorscheme rose-pine'
+  end,
+}
