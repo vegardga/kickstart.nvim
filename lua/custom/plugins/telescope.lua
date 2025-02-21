7e101cbdad69f22b1151ecb6bc73ffe1814a831f
Vegard Gamnes <vegard@gamnes.no>
2025-02-21 08:40:44 +0100

    Add ctrl+p for git_files telescope

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -82,6 +82,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
     vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = '[ ] Search Files' })
+    vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Search Git Files' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
